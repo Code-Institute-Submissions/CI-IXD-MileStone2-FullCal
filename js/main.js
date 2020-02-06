4df0588be4895f141d21a712645e0cd22aab5f37
Ross Coe <rossgeography@yahoo.com>
2020-02-06 11:47:35 +0000

    Events vs Favourites sources bug

--- js/main.js
+++ js/main.js
@@ -14,8 +14,8 @@ class App {
     this.$calendar = document.querySelector("#calendar")
     this.$favIcon = document.querySelector(".favourite")
     this.$selecta = document.querySelector("#color_selector")
-    this.$categories = document.querySelector("#checkboxes")
-    this.cats = new Set
+    this.$checkboxes = document.querySelector("#checkboxes")
+    this.categories = new Set
 
     const App = this
     App.calendar = new FullCalendar.Calendar(App.$calendar, { //const calendarEl = this.$calendar
@@ -27,21 +27,29 @@ class App {
 
           eventRender: function (info) {
          
-
-
-            return App.cats.has(info.event.extendedProps.category)? true : false
+            return App.categories.has(info.event.extendedProps.category)? true : false
+         
+            // return true
             
-      
           },
         eventClick: function(info){
           info.jsEvent.preventDefault() // don't let the browser navigate
           App.renderEventModal(info)
         },
-        events:  async function(fetchInfo,successCallback){
-              
+        events:   async function(fetchInfo,successCallback){
+              /* event source 1 FETCH */
                await App.getEvents()
-               App.printUnique(App.cats)
-                successCallback(App.events)
+               App.printCheckboxes(App.categories)
+              successCallback(App.events)
+                
+              /*
+              event source 2
+               const ents = [{"id":"873612231","title":"Besties, Dark Waters & What Next Mother- Film Premiere","start":"2020-02-07T19:30:00+00:00","url":"https://wexfordartscentre.ticketsolve.com/shows/873612231/","classNames":["presentationcentre","wexfordartscentre"],"extendedProps":{"description":"\n          <p><span>Three New Short Wexford Films to be screened in aid of FOCUS Ireland. The World Premiere of a new short film,&nbsp;<strong>BESTIES</strong>, featuring members of the Enniscorthy Drama Group, will be shown in the Presentation Centre, on Friday 7<sup>th</sup>&nbsp;of February, at 7.30pm in aid of FOCUS Ireland.The cast includes, Karen Franklin, Jennafer Boyd, Fintan Kelly, Maeve Ennis, Summer Keane and Jennifer Kelly. Written and directed by Dick Donaghue and produced by Jer Ennis. Filmed in Enniscorthy.</span></p>\n<p><span>Two other short films will also be premiered. </span><strong><span>DARK WATERS</span></strong><span>, starring Sharon Griffiths with David Parsons which was filmed in Bridgetown. </span><strong><span>WHAT NEXT MOTHER</span></strong><span>, a comedy, filmed in Bunclody. Starring Mary Gibson, Elaine Jordan, Niall Kennedy and Lauren Jordan, will feature on the night also.</span></p>\n<p><span>All proceeds will be donated to FOCUS Ireland, the aim of the night is to shine a light on the homelessness issue, raise much needed funds and also to show off the writing and acting talent in our locality</span></p>\n<p><strong>&nbsp;</strong></p>\n<p><strong>&nbsp;</strong></p>\n        ","category":"Film","images":{"thumb":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/42108e2ccaa9cbeaed96ff70f4f71abb3e953a28352072c0afd65721f1c07e2b","medium":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/db985b134426e3a0a042b6dc139e02b7924a669da4f6434c9c9c4b9553d63a33","large":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/d82b4c5034021c15162868846a860fc142237fb62f146ef228d6181ef8a17941"}}}]
+              console.log(ents)
+              successCallback(ents)
+              */
+
+
             //  successCallback([{ title: "Slow Flow Mindful Yoga" , start: "2020-02-07T19:30:00+00:00" }])
               } 
       //end App.Cal{}     
@@ -61,12 +69,11 @@ class App {
     //   console.log("change")
     //   App.calendar.rerenderEvents()      // App.FullCalender.render()
     //  })
-    App.$categories.addEventListener("change", function() {
+    App.$checkboxes.addEventListener("change", function() {
      
       let checkboxes = [...this.querySelectorAll("input[type=checkbox")].filter(x => x.checked ).map(x => x.value)
-      App.cats = new Set(checkboxes)
-      // console.log(App.cats)
-      // App.cats = [...checkboxes.values]
+      App.categories= new Set(checkboxes)
+
       App.calendar.rerenderEvents()  
     })
   }
@@ -85,7 +92,7 @@ class App {
                 tags.push(tag.textContent)
               })
               const category = show.querySelector("event_category").textContent
-              App.cats.add(category)
+              App.categories.add(category)
               const showObj = {
                 id: show.getAttribute("id"),
                 title: show.querySelector("name").childNodes[1].nodeValue, // title: show.getElementsByTagName("name")[0].childNodes[1].nodeValue,
@@ -136,8 +143,6 @@ class App {
          
 
             })
-
-
           
           $('#modalTitle').html(info.event.title)
           $('#modalBody').html(`
@@ -153,40 +158,21 @@ class App {
 
           console.log("Event Id: "+info.event.id)
 
-    
           return false
         }
 
-    printCheckBoxes(x){
 
-      $('<input />', {
-        'type': 'checkbox',
-        'value': x,
-        
-        'name': 'someName'
-    }).after(x).appendTo(this.$categories);
-      // console.log(cb)
-      // .append(cb)
-
- 
-    }
-    printUnique(arr){
-      
-      // let unique = [...new Set(arr)]; 
-      // console.log(unique)
-
-      this.cats.forEach( x =>  {
+    printCheckboxes(categories){
+      this.categories.forEach( x =>  {
         $('<input />', {
           'type': 'checkbox',
           'value': x,
           // 'checked': 'checked',
           'name': 'someName'
       }).prop("checked", true) 
-        .wrap('<label class="mr-2"></label>').closest('label').append('<span class="ml-2">'+x+'</span>').appendTo(this.$categories);
+        .wrap('<label class="mr-2"></label>').closest('label').append('<span class="ml-2">'+x+'</span>').appendTo(this.$checkboxes);
         });
      
-
-
     }
       
     renderCal(){
@@ -209,7 +195,7 @@ class App {
     addEvent(newEvent){ // destructor note{}
        console.log("passed: "+JSON.stringify(newEvent))
 
-          newEvent.isFavourited = "true"
+          newEvent.extendedProps.isFavourited = "true"
           //console.log(JSON.stringify(this.favourites))
           this.favourites = [...this.favourites, newEvent ] //nesting! [] {...newEvent, isFavourited: "true"}
           console.log(this.favourites)
