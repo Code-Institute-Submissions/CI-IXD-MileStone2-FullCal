9017779997976950180b2a9e8cbe56c611a87b2c
Ross Coe <rossgeography@yahoo.com>
2020-02-08 13:59:40 +0000

    Categories Map implemented - checkboxes started

--- js/main.js
+++ js/main.js
@@ -4,95 +4,100 @@ class App {
     console.log('app works!')  
     //this.notes = []
     this.favourites = JSON.parse(localStorage.getItem("favourites")) || []
-    this.events = []
-    this.origin = "https://wexfordartscentre.ticketsolve.com/shows.xml"
-    this.proxy  = "https://cors-anywhere.herokuapp.com/"
+    // this.events = []
+    // this.origin = "https://wexfordartscentre.ticketsolve.com/shows.xml"
+    // this.proxy  = "https://cors-anywhere.herokuapp.com/"
     // this.proxy = "https://cors.x7.workers.dev/"
     let URL = window.location.href
     this.local = URL.substring(0, URL.lastIndexOf("/") + 1) + "shows.xml";
-
+    this.cats = false
     this.$calendar = document.querySelector("#calendar")
     this.$favIcon = document.querySelector(".favourite")
     this.$selecta = document.querySelector("#color_selector")
+    this.$checkAll = document.querySelector("#checkAll")
     this.$checkboxes = document.querySelector("#checkboxes")
-    this.categories = new Set
+    
+    this.categories = new Map
 
     const App = this
     App.calendar = new FullCalendar.Calendar(App.$calendar, { //const calendarEl = this.$calendar
-        
+
       locale: 'en-gb',
         plugins: [ 'dayGrid', 'list', 'bootstrap'],
         themeSystem: 'bootstrap', 
         // defaultView: 'list',
-
+    
+        
           eventRender: function (info) {
-         
-            return App.categories.has(info.event.extendedProps.category)? true : false
+
+            return App.categories.get((info.event.extendedProps.category))
          
             // return true
             
           },
+
+
         eventClick: function(info){
           info.jsEvent.preventDefault() // don't let the browser navigate
           App.renderEventModal(info)
         },
         events:   async function(fetchInfo,successCallback){
               /* event source 1 FETCH */
-               await App.getEvents()
-               App.printCheckboxes(App.categories)
-              successCallback(App.events)
+              let result = await App.getEvents()
+                App.printCheckboxes()
+                // App.checkCheckboxes()
+
+              successCallback( result ) 
                 
               /*
-              event source 2
-               const ents = [{"id":"873612231","title":"Besties, Dark Waters & What Next Mother- Film Premiere","start":"2020-02-07T19:30:00+00:00","url":"https://wexfordartscentre.ticketsolve.com/shows/873612231/","classNames":["presentationcentre","wexfordartscentre"],"extendedProps":{"description":"\n          <p><span>Three New Short Wexford Films to be screened in aid of FOCUS Ireland. The World Premiere of a new short film,&nbsp;<strong>BESTIES</strong>, featuring members of the Enniscorthy Drama Group, will be shown in the Presentation Centre, on Friday 7<sup>th</sup>&nbsp;of February, at 7.30pm in aid of FOCUS Ireland.The cast includes, Karen Franklin, Jennafer Boyd, Fintan Kelly, Maeve Ennis, Summer Keane and Jennifer Kelly. Written and directed by Dick Donaghue and produced by Jer Ennis. Filmed in Enniscorthy.</span></p>\n<p><span>Two other short films will also be premiered. </span><strong><span>DARK WATERS</span></strong><span>, starring Sharon Griffiths with David Parsons which was filmed in Bridgetown. </span><strong><span>WHAT NEXT MOTHER</span></strong><span>, a comedy, filmed in Bunclody. Starring Mary Gibson, Elaine Jordan, Niall Kennedy and Lauren Jordan, will feature on the night also.</span></p>\n<p><span>All proceeds will be donated to FOCUS Ireland, the aim of the night is to shine a light on the homelessness issue, raise much needed funds and also to show off the writing and acting talent in our locality</span></p>\n<p><strong>&nbsp;</strong></p>\n<p><strong>&nbsp;</strong></p>\n        ","category":"Film","images":{"thumb":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/42108e2ccaa9cbeaed96ff70f4f71abb3e953a28352072c0afd65721f1c07e2b","medium":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/db985b134426e3a0a042b6dc139e02b7924a669da4f6434c9c9c4b9553d63a33","large":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/d82b4c5034021c15162868846a860fc142237fb62f146ef228d6181ef8a17941"}}}]
-              console.log(ents)
-              successCallback(ents)
-              */
+              event source 2 */
+              //  const ents = [{"id":"873612231","title":"Besties, Dark Waters & What Next Mother- Film Premiere","start":"2020-02-07T19:30:00+00:00","url":"https://wexfordartscentre.ticketsolve.com/shows/873612231/","classNames":["presentationcentre","wexfordartscentre"],"extendedProps":{"description":"\n          <p><span>Three New Short Wexford Films to be screened in aid of FOCUS Ireland. The World Premiere of a new short film,&nbsp;<strong>BESTIES</strong>, featuring members of the Enniscorthy Drama Group, will be shown in the Presentation Centre, on Friday 7<sup>th</sup>&nbsp;of February, at 7.30pm in aid of FOCUS Ireland.The cast includes, Karen Franklin, Jennafer Boyd, Fintan Kelly, Maeve Ennis, Summer Keane and Jennifer Kelly. Written and directed by Dick Donaghue and produced by Jer Ennis. Filmed in Enniscorthy.</span></p>\n<p><span>Two other short films will also be premiered. </span><strong><span>DARK WATERS</span></strong><span>, starring Sharon Griffiths with David Parsons which was filmed in Bridgetown. </span><strong><span>WHAT NEXT MOTHER</span></strong><span>, a comedy, filmed in Bunclody. Starring Mary Gibson, Elaine Jordan, Niall Kennedy and Lauren Jordan, will feature on the night also.</span></p>\n<p><span>All proceeds will be donated to FOCUS Ireland, the aim of the night is to shine a light on the homelessness issue, raise much needed funds and also to show off the writing and acting talent in our locality</span></p>\n<p><strong>&nbsp;</strong></p>\n<p><strong>&nbsp;</strong></p>\n        ","category":"Film","images":{"thumb":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/42108e2ccaa9cbeaed96ff70f4f71abb3e953a28352072c0afd65721f1c07e2b","medium":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/db985b134426e3a0a042b6dc139e02b7924a669da4f6434c9c9c4b9553d63a33","large":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/d82b4c5034021c15162868846a860fc142237fb62f146ef228d6181ef8a17941"}}}]
+              // console.log(ents)
+              // successCallback(ents)
+              /**/
 
 
             //  successCallback([{ title: "Slow Flow Mindful Yoga" , start: "2020-02-07T19:30:00+00:00" }])
-              } 
+              },
+    
       //end App.Cal{}     
       })
 
 
     //this.getEvents()
-    this.renderCal()
+    // this.renderCal()
+    this.calendar.render()
+    
+    // console.log("first")
+    
     this.addEventListeners();
+
+    // this.$nextButton = document.querySelector(".fc-next-button")
     }
 
 
-  addEventListeners(){
-    const App = this
-    // App.$selecta.addEventListener('change', function() { // this changes cntx
-    //   //console.log(this.options[this.selectedIndex].value)
-    //   console.log("change")
-    //   App.calendar.rerenderEvents()      // App.FullCalender.render()
-    //  })
-    App.$checkboxes.addEventListener("change", function() {
-     
-      let checkboxes = [...this.querySelectorAll("input[type=checkbox")].filter(x => x.checked ).map(x => x.value)
-      App.categories= new Set(checkboxes)
-
-      App.calendar.rerenderEvents()  
-    })
-  }
-  
-  async getEvents(){
+    async getEvents(){
       const App = this
-     await fetch(this.proxy+this.origin)
-    // await fetch(this.local)
-              .then( response => response.text())
-              .then( function(data){
-                console.log("fetched data ok")
-              const xmlDOM = new DOMParser().parseFromString(data, 'text/xml') // DOM tree from XML
-              xmlDOM.querySelectorAll("show").forEach(show => { 
+      const events = []
+      //  await fetch(this.proxy+this.origin)
+      await fetch(this.local)
+            .then( response => response.text())
+            .then( function(data){
+            // console.log("fetched data ok")
+            
+            const xmlDOM = new DOMParser().parseFromString(data, 'text/xml') // DOM tree from XML
+            xmlDOM.querySelectorAll("show").forEach(show => { 
               const tags = []
-              $(show).find("tag").get().forEach(tag => {
-                tags.push(tag.textContent)
-              })
-              const category = show.querySelector("event_category").textContent
-              App.categories.add(category)
+              $(show).find("tag").get().forEach(tag =>  tags.push(tag.textContent) )
+              
+              let category = show.querySelector("event_category").textContent
+              if(category.toLowerCase().includes("children") || category.toLowerCase().includes("workshop")) {
+                category = "Children"
+              }
+              if(!App.categories.has(category)){
+                App.categories.set(category, true)
+              }
+              
               const showObj = {
                 id: show.getAttribute("id"),
                 title: show.querySelector("name").childNodes[1].nodeValue, // title: show.getElementsByTagName("name")[0].childNodes[1].nodeValue,
@@ -108,15 +113,62 @@ class App {
                     large: show.querySelector("url[size='large']").innerHTML
                   }
                 }
-
               }
-              console.log(showObj)
-              App.events.push(showObj)
-              })
-              return App.events
+          events.push(showObj)
           })
-          // .catch()
-    }
+        })
+        // .catch()
+  
+            return events
+      }
+
+  addEventListeners(){
+    const App = this
+
+    App.$checkboxes.addEventListener("change", function(event) {
+      
+      let checkboxes = [...this.querySelectorAll("input[type=checkbox]")].slice(1)
+      
+      App.categories.clear()
+
+      const cb = event.target.closest("input[type=checkbox]")
+      console.log(cb.value)
+      if(cb.value != "all"){
+        console.log("All not checked")
+        $("#checkAll").prop("indeterminate", true)
+        checkboxes.forEach(x => {
+          App.categories.set(x.value, x.checked)
+        })
+      }else if(cb.value == "all" && cb.checked == true){
+        $("input:checkbox").prop("checked", true)
+        console.log("checkAll On")
+        checkboxes.forEach(x => {
+          App.categories.set(x.value, true)
+        }) 
+      } else {
+        console.log("unchecked All?")
+        // randomly ste a cate
+      }
+      
+
+      // if(App.$checkAll.checked){
+        
+       
+      // }else{
+      //   // $("#checkAll").prop("indeterminate", true)
+      //   console.log("checkAll Off")
+        
+      
+      
+      App.calendar.rerenderEvents()  
+
+    })
+
+
+    // App.$nextButton.addEventListener("click", function() {console.log('next!')})
+
+  }
+  
 
       getFavourites(){
         console.log(this.favourites)
@@ -162,23 +214,25 @@ class App {
         }
 
 
-    printCheckboxes(categories){
-      this.categories.forEach( x =>  {
-        $('<input />', {
-          'type': 'checkbox',
-          'value': x,
-          // 'checked': 'checked',
-          'name': 'someName'
-      }).prop("checked", true) 
-        .wrap('<label class="mr-2"></label>').closest('label').append('<span class="ml-2">'+x+'</span>').appendTo(this.$checkboxes);
-        });
-     
-    }
-      
-    renderCal(){
 
-          this.calendar.render()
-     }
+    printCheckboxes(){
+      if (this.cats == false) {
+          this.categories.forEach( (v, k) =>  {
+          $('<input />', {
+            'type': 'checkbox',
+            'value': k,
+            'name': 'someName'
+          })
+          .prop("checked", v) 
+          .wrap('<label class="mr-2"></label>')
+          .closest('label')
+          .append('<span class="ml-2">'+k+'</span>')
+          .appendTo(this.$checkboxes);
+          })
+          this.cats = true
+      }
+    }
+
 
 
     // Methods
