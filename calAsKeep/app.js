7696b27d604075bcfda88591cb84bcee56fd450b
Ross Coe <rossgeography@yahoo.com>
2020-02-24 21:38:59 +0000

    listView and bottom table working - side FavCal gone

--- calAsKeep/app.js
+++ calAsKeep/app.js
@@ -1,8 +1,6 @@
 import {default as getEvents, categories} from "../components/Events.js"
-import checkFavourites from "../utils/checkFavourites.js" // helper func
+
 import Store from "../js/store.js"
-
-
 
 // Event Class: Represents an Event
 // consturctir of Book - properties??
@@ -58,8 +56,7 @@ class UI {
         cell.parentElement.remove()
       }
     })
-    // if(el.classList.contains('delete')) {
-      // el.parentElement.parentElement.remove(); // tr not just td
+
   }
 
   static showAlert(message, className){
@@ -77,15 +74,11 @@ class UI {
 
     // $('#fullCalModal').modal("toggle")
 
-
-
   }
 
 }
 
 // Store Class - localStorage
-
-
 
 // Events: Display Fav Cal
 document.addEventListener("DOMContentLoaded", UI.displayEvents)
@@ -104,9 +97,8 @@ const sources = {
   },
 }
 
-const  favourites  = Store.getFavEvents()
+const favourites  = Store.getFavEvents()
 
-// document.addEventListener("DOMContentLoaded", function() {
 
   let $calendar = document.querySelector("#calendar")
   let calendar = new FullCalendar.Calendar($calendar, {
@@ -117,17 +109,7 @@ const  favourites  = Store.getFavEvents()
     //  defaultView: 'dayGridMonth',
      defaultView: 'listWeek',
      themeSystem: 'bootstrap', // black table borders! 
-   
-    //  customButtons: {
-    //    favourites: {
-    //      text: 'Favs!',
-    //      click: function(){   // turn on all cats!
-    //       //  calendar.getEventSourceById(1).remove()
-    //       //  calendar.addEventSource(sources.favs)
-    //       calendar.gotoDate(new Date(closest)) //calendar.gotoDate("2021-01-09")
-    //      }
-    //    }
-    //  },
+
      header: {
        left: 'title, dayGridMonth, listWeek',
        center: '',
@@ -135,36 +117,17 @@ const  favourites  = Store.getFavEvents()
       },
       // eventSources: [sources.favs],
      views: {
-    //   dayGridMonth: { // name of view
-    //     // titleFormat: { year: 'numeric', month: '2-digit', day: '2-digit' }
-    //     // other view-specific options here
-    //     eventTimeFormat: {
-    //     hour: 'numeric',
-    //     minute: '2-digit',
-    //     meridiem: true,
-    //     omitZeroMinute: true,
-    //     hour12: true
-    //     },
-    //   }
+        listWeek: {
 
-
-    listWeek: {
-
-        eventTimeFormat: {
-        hour: 'numeric',
-        minute: '2-digit',
-        meridiem: true,
-        // omitZeroMinute: true,
-        hour12: true
-    }
-  },
-},
-
-
-
-
-
-
+            eventTimeFormat: {
+            hour: 'numeric',
+            minute: '2-digit',
+            meridiem: true,
+            // omitZeroMinute: true,
+            hour12: true
+        }
+      },
+    },
     eventRender: function ({event, el, jsEvent, view}) {
      
       const isFavourited = Store.checkFavs(event.id)      
@@ -230,6 +193,26 @@ const  favourites  = Store.getFavEvents()
     eventPositioned: function(info){
       
       info.el.querySelector(".favourite").setAttribute("data-show", `${info.event.extendedProps.jdata}`) //IDS can't be numbers!
+      info.el.querySelector(".favourite").addEventListener("click", e => {
+        const show = JSON.parse(e.target.dataset.show)
+        console.log(show)
+        const isFavourited = Store.checkFavs(show.id);
+            console.log(isFavourited)
+            if(isFavourited) {
+              Store.removeEvent(show.id)
+              UI.showAlert("Event Removed to Favourites", "warning")
+              UI.deleteEventFromList(show.id)
+              // calendar.rerenderEvents()
+            }else{
+                  UI.addEventToList(show)
+                  Store.addEvent(show)
+                  // calendar.rerenderEvents()
+                  // favCal.rerenderEvents()
+                  UI.showAlert("Event Added to Favourites", "success")  
+                  // UI.clearFields()
+            }
+          calendar.rerenderEvents()
+      })
 
 
     },
@@ -255,123 +238,20 @@ const  favourites  = Store.getFavEvents()
     // printCheckboxes()
     // intervalStart = calendar.view.currentStart
     // favCal() // T/F flag if create
-    successCallback(await result) 
-    addEventListeners()
+    successCallback(await result)
+    
     },
 
      
    })
 
- 
-
-  let $favCal = document.querySelector("#favCal")
-
-  var intervalStart
-
-
-  let favCal = new FullCalendar.Calendar($favCal, {
-    header: { center: '', right: 'prev,next' }, // buttons for switching between views
-    plugins: [ 'dayGrid', 'list', 'bootstrap', 'timeGrid'],
-    // defaultDate: intervalStart,
-    // defaultView: 'list',
-    // views: {
-    //   timeGridFourDay: {
-    //     titleFormat: { year: 'numeric', month: 'short', day: 'numeric' },
-    //     type: 'list',
-    //     // start: intervalStart, // customStart??
-    //     duration: { days: 21 },
-    //     buttonText: '4 day'
-    //   }
-    // },
-    locale: 'en-gb',
-    defaultView: 'listWeek',
-    // defaultDate: intervalStart,
-    
-    // events: function(fetchInfo, successCallback){
-    //   // [{"id":"873612231","title":"Besties, Dark Waters & What Next Mother- Film Premiere","start":"2020-02-07T19:30:00+00:00","url":"https://wexfordartscentre.ticketsolve.com/shows/873612231/","classNames":["presentationcentre","wexfordartscentre"],"extendedProps":{"description":"\n          <p><span>Three New Short Wexford Films to be screened in aid of FOCUS Ireland. The World Premiere of a new short film,&nbsp;<strong>BESTIES</strong>, featuring members of the Enniscorthy Drama Group, will be shown in the Presentation Centre, on Friday 7<sup>th</sup>&nbsp;of February, at 7.30pm in aid of FOCUS Ireland.The cast includes, Karen Franklin, Jennafer Boyd, Fintan Kelly, Maeve Ennis, Summer Keane and Jennifer Kelly. Written and directed by Dick Donaghue and produced by Jer Ennis. Filmed in Enniscorthy.</span></p>\n<p><span>Two other short films will also be premiered. </span><strong><span>DARK WATERS</span></strong><span>, starring Sharon Griffiths with David Parsons which was filmed in Bridgetown. </span><strong><span>WHAT NEXT MOTHER</span></strong><span>, a comedy, filmed in Bunclody. Starring Mary Gibson, Elaine Jordan, Niall Kennedy and Lauren Jordan, will feature on the night also.</span></p>\n<p><span>All proceeds will be donated to FOCUS Ireland, the aim of the night is to shine a light on the homelessness issue, raise much needed funds and also to show off the writing and acting talent in our locality</span></p>\n<p><strong>&nbsp;</strong></p>\n<p><strong>&nbsp;</strong></p>\n        ","category":"Film","images":{"thumb":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/42108e2ccaa9cbeaed96ff70f4f71abb3e953a28352072c0afd65721f1c07e2b","medium":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/db985b134426e3a0a042b6dc139e02b7924a669da4f6434c9c9c4b9553d63a33","large":"https://dc40ra2rfm3rp.cloudfront.net/as-assets/variants/LXoGhVKjEdUogGVvXQNMp1UB/d82b4c5034021c15162868846a860fc142237fb62f146ef228d6181ef8a17941"}}}]
-    //   const ents  = Store.getFavEvents()
-    //   successCallback(ents)
-    // },
-    eventSources: [sources.favs],
-    datesRender: function({view, el}) {
-      // intervalStart = view.currentStart
-      // console.log(intervalStart)
-    },
-    views: {
-      listWeek: {
-        // noEventsMessage: "Non " + Store.getFavEvents().length + " Future Events " + intervalStart ,
-      }
-    },
-  })
-
-  // console.log(timeBetween - favCal.view.currentStart)
-
-
-
-
- 
-
-
-
-
-//    function openForm(event){
-//     //  console.log(event.extendedProps.jdata)
-    
-//     $('#modalTitle').html(event.title)
-//     $('#modalBody').html(`
-//       <p class="edate">${event.start}</p>
-//       <p class="eid">${event.id}</p>
-//     `)
-//     $('#modalBody').html(`
-//       <img src="${event.extendedProps.images.medium}" style="object-fit: cover; object-position: 20% 10%;" alt="${event.title}" />
-//       <div class="card-body">
-//       ${event.extendedProps.description} 
-//       </div>
-//     `);
-//     $('.favourite').attr('data-event', event.extendedProps.jdata )
-//     const isFavourited = Store.checkFavs(event.id)
-//     $('.favourite-text').html(`${isFavourited? "Remove from Favourites" : "Add to Favourites" }`)  // Event Added Msg on both
-//     $('#eventUrl').attr('href',event.url)
-//     $('#fullCalModal').modal()
-//   // document.querySelector("#form").classList.add("form-open")
-//   //     document.querySelector("#note-title").style.display = "block"
-//   //     document.querySelector("#note-title").value = info.event.title
-//   //     document.querySelector("#note-text").value = info.event.extendedProps.description
-//   //     document.querySelector("#form-buttons").style.display = "block"
-// }
-
 function addEventListeners() {
-// JSON.parse(document.querySelector(".favourite").getAttribute("data-event"))
-const $favourites = [...document.querySelectorAll(".favourite")]
-console.log($favourites)
-$favourites.forEach(favourite => {
-  favourite.addEventListener("click", (e) => {
 
-    const event = JSON.parse(e.target.dataset.show)
-    const isFavourited = Store.checkFavs(event.id);
-    console.lof(isFavourited)
-    if(isFavourited) {
-      Store.removeEvent(event.id)
-      UI.showAlert("Event Removed to Favourites", "warning")
-      UI.deleteEventFromList(event.id)
-      // calendar.rerenderEvents()
-    }else{
+// const $favourites = [...document.querySelectorAll(".favourite")]
+// for(const $favourite of $favourites){
 
-      
-          UI.addEventToList(event)
-        
-          Store.addEvent(event)
-      
-          
-          calendar.rerenderEvents()
-          // favCal.rerenderEvents()
-          UI.showAlert("Event Added to Favourites", "success")
-      
-          UI.clearFields()
-    }
-  }
-  )
-})
+ 
+
 
 
   // Event to Add Event = UI and Storage
@@ -391,35 +271,16 @@ $favourites.forEach(favourite => {
 
     //
 
+    calendar.rerenderEvents()
   })
 
-  calendar.render()
 
 }
 
-async function nextEvent(){
-  await favCal.render()
-  const ents  = Store.getFavEvents() 
-  const now = new Date()
-  ents.forEach( d => {
-    const date = new Date(d.start)
-    // console.log(date)
-    if (date >= now && (date < new Date(closest) || date < closest)){
-      closest = d.start
-    }
-  })
-  // console.log(closest)
-  closest = new Date(closest)
-  if(document.querySelector(".fc-list-empty")){
 
-    document.querySelector(".fc-list-empty").innerHTML = `<a href="#" class="btn">${closest}</a>`
-    document.querySelector(".fc-list-empty").addEventListener("click", () => {
-      favCal.gotoDate(closest)
-    
-    })
-
-  }
-}
 calendar.render()
-nextEvent()
+addEventListeners()
+
+
+
 
